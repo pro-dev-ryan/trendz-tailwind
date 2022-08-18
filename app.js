e40e9d9b7e25eb8b1ef5b3d2970560af58a1a1e3
pro-dev-ryan <azizul.developer@gmail.com>
2022-08-18 15:10:44 +0600

    js added

---
+++ app.js
@@ -0,0 +1,20 @@
+function makePenguin (){
+    document.getElementById('penguinField').innerText = "I'm The penguin";
+}
+
+function btnFunction (){
+const getData = document.getElementById('')
+}
+
+
+
+
+function discount (){
+    const getValue = document.getElementById('price1');
+    if ()
+
+}
+
+
+// modal body
+const modalBody = document.getElementById('discount');
